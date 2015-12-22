396558c0247f98b7e93cd015e6bcd7019aa5cb82
Darrell Richards <hello@darrellrichards.com>
2015-12-22 14:34:10 -0500

    Added Posts and Bootstrapp-sass Gem

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'devise'
+gem 'bootstrap-sass', '~> 3.3.6'
 gem 'sdoc', '~> 0.4.0', group: :doc
 group :development, :test do
   gem 'byebug'
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,28 @@
 class PostsController < ApplicationController
-	# before_action :authenticate_user!
 	def index
+		@posts = Post.all.order('created_at DESC')
 	end
+	def new
+		@post = Post.new
+	end
+	def create
+		@post = Post.new(post_params)
+		if @post.save
+			redirect_to @post
+		else 
+			render 'new'
+		end
+	end	
+	def show
+		@post = Post.find(params[:id])
+	end
+	def edit 
+		@post = Post.find(params[:id])
+	end
+	def update
+	end
+	private
+		def post_params
+			params.require(:post).permit(:title, :body)
+		end
 end
