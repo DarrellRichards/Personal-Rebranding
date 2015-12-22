0eed4b398389e2f54310968d50243f3c82374d1b
Darrell Richards <hello@darrellrichards.com>
2015-12-22 14:49:08 -0500

    Made a page controllers and updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get '/about' => 'pages#about', as: 'about'
+  get '/contact' => 'pages#contact', as: 'contact'
+  get '/work' => 'pages#work', as: 'work'
+
   devise_for :users
   resources :posts
   root "home#index"
