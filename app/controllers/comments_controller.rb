8f1b5fcbfd629ef8bc261308f9943a150a6bd71b
Darrell Richards <hello@darrellrichards>
2015-12-27 16:42:51 -0500

    Fixed error with comments with friendly_id gem

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,11 @@
 class CommentsController < ApplicationController
   def create
-    @post = Post.find(params[:post_id])
+    @post = Post.friendly.find(params[:post_id])
     @comment = @post.comments.create(params[:comment].permit(:name, :body))
     redirect_to post_path(@post)
   end
   def destroy
-    @post = Post.find(params[:post_id])
+    @post = Post.friendly.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
     @comment.destroy
 
